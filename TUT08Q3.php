a21d43652c145e2f793aac65e6c0305ec07c63ad
Nima Saed <nima.saed@me.com>
2016-01-14 15:34:44 +0800

    TUT 08 Q3 added

---
+++ TUT08Q3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+
+      <?php
+        $string = "Hello";
+        echo "<script type=\"text/javascript\"> alert(\"$string\");</script>";
+       ?>
+
+  </head>
+  <body>
+
+  </body>
+</html>
